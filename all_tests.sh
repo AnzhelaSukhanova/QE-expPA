9d8657502f3437b62e18ee8da0e63a08c891ab7a
Kirill K. Smirnov <kirill.k.smirnov@gmail.com>
2021-02-17 18:33:14 +0300

    Eliminate bash-isms
    Signed-off-by: Kirill K. Smirnov <kirill.k.smirnov@gmail.com>

--- all_tests.sh
+++ all_tests.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 cd tests
 IN='*.in'
 
 for i in $IN
     do
-        ./../main $i "${i::-2}"'out'
+        ../main $i "`basename $i .in`.out"
     done
-    
-cd ../
+cd ..
